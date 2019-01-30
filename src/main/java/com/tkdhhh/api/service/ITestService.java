2531c79eafc8233fc1c66ed26b970f60d292f865
tkdhhh <814534792@qq.com>
2019-01-30 17:47:37 +0800

    第一次提交测试

---
+++ src/main/java/com/tkdhhh/api/service/ITestService.java
@@ -0,0 +1,13 @@
+package com.tkdhhh.api.service;
+
+import com.tkdhhh.api.entity.Question;
+
+public interface ITestService {
+
+    /*
+    *author: dh
+    *date:2019/1/22
+    *describe:
+    */
+    Question testReturn(int id);
+}
